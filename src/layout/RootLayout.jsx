149fda62988d72da68a97a0ea5a9082c0e6e3ad3
sowho.j <jangseohu112@gmail.com>
2026-07-04 22:46:43 +0900

    🎉 Start: Start New Project

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,9 @@
+RootLayout.jsx;
 import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
 
 export default function RootLayout() {
   return (
     <>
-      <Header />
       <main>
         <Outlet />
       </main>
