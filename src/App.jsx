05b2868d3cef33933a9fc670145872fc1d84cb44
sowho.j <jangseohu112@gmail.com>
2026-07-07 10:11:36 +0900

    🐛 Fix: 루트 파일 명 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "../src/layout/RootLayout.jsx";
-import Main from "../src/pages/Main/Main.jsx";
+import Home from "../src/pages/Home/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Home />} />
           <Route path="/add" element={<></>} />{" "}
           {/* 추후 element에 상품 등록 페이지 들어가야함 */}
         </Route>
