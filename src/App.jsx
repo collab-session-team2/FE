28c5c6569260cccd371b2a43aec3e96e11340bd8
sowho.j <jangseohu112@gmail.com>
2026-07-07 11:38:15 +0900

    ✨ Feat:라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
 import Home from "./pages/Home/Home";
 import RoomCreate from "./pages/roomCreate/RoomCreate";
-
+import DiaryMain from "./pages/diary/diaryMain";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +11,7 @@ function App() {
         <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<RoomCreate />} />
+          <Route path="/diaryMain" element={<DiaryMain />} />
         </Route>
       </Routes>
     </BrowserRouter>
